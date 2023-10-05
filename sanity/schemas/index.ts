04862db6d3562c27344aca2901682378e879261c
alejandra-rojas <alejandra.rojas@hyperisland.se>
2023-10-05 18:36:39 +0200

    Adding hello schema

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -1,6 +1,7 @@
+import hello from "./hello-schema";
 import page from "./page-schema";
 import project from "./project-schema";
 
-const schemas = [project, page];
+const schemas = [project, page, hello];
 
 export default schemas;
